82848052d099170f9dff817e0f59365771986740
SnehaChaudhary01 <cdysneha55@gmail.com>
2026-06-25 13:58:35 +0545

    Add:Bio data

---
+++ basics/bio.cpp
@@ -0,0 +1,18 @@
+//biodata using variables 
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"---------BIO DATA-------------"<<endl;
+    //assigning variables
+    string first_name = "Sneha";
+    string last_name ="Chaudhary";
+    int age = 19;
+    string faculty = "BSC.CSIT";
+    string college_name = "Butwal Multiple Campus";
+    //outputs
+    cout << "My name is" << ' ' << first_name <<" "<<last_name <<"."<< endl;
+    cout << "I am" << " " << age << " " "years old." << endl;
+    cout << "I am a" <<" "<<faculty <<" ""Student." << endl;
+    cout << "My college name is " <<college_name <<"."<< endl;
+    return 0;
+}
