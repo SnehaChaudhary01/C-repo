2aae6338319e9490f71176a3be0c39f77e1483dc
SnehaChaudhary01 <cdysneha55@gmail.com>
2026-07-04 08:52:11 +0545

    Add swap.cpp

---
+++ dsa/array/swap.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+void swapMaxMin(int num[], int size) {
+    int maxIndex = 0;
+    int minIndex = 0;
+
+    for (int i = 1; i < size; i++) {
+        if (num[i] > num[maxIndex])
+            maxIndex = i;
+
+        if (num[i] < num[minIndex])
+            minIndex = i;
+    }
+
+    swap(num[maxIndex], num[minIndex]);
+}
+
+int main() {
+    int num[] = {1, 5, 7, 4};
+    int size = 4;
+
+    swapMaxMin(num, size);
+
+    for (int i = 0; i < size; i++)
+        cout << num[i] << " ";
+}
