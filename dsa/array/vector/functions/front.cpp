e559276169404835fda8fe762f67c99c28b3d651
SnehaChaudhary01 <cdysneha55@gmail.com>
2026-07-07 09:14:34 +0545

    Add front.cpp

---
+++ dsa/array/vector/functions/front.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector <int> vec;
+    vec.push_back(25);
+    vec.push_back(26);
+    vec.push_back(27);
+    cout << "Size after push back:" << vec.size() << endl;
+    cout << vec.front() << endl;
+    return 0;
+}
