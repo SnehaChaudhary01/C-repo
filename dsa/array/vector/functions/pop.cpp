716f7f3333363362e8c97cd3a84d689424cf5a1d
SnehaChaudhary01 <cdysneha55@gmail.com>
2026-07-07 09:14:54 +0545

    Add pop.cpp

---
+++ dsa/array/vector/functions/pop.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector <int> vec;
+    vec.push_back(25);
+    vec.push_back(26);
+    vec.push_back(27);
+    cout << "Size after push back:" << vec.size() << endl;
+
+    vec.pop_back(); 
+    
+    for(int value : vec ){
+        cout <<  "Value :" <<value << endl;
+    }
+    return 0;
+}
